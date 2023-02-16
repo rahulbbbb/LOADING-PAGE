57fda5b6ea35c4bde29725f62557cd4797558596
rahulbbbb <124687528+rahulbbbb@users.noreply.github.com>
2023-02-16 18:01:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function blurring() {
 
   loadText.innerText = `${load}%`
   loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+  bg.style.filter = `blur(${scale(load, 0, 100,  0)}px)`
 }
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
